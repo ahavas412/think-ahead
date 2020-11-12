66eb7a8898d986851df0875cb1f0e07b43984e0f
Augustin <ahavas412@gmail.com>
2020-11-12 16:18:35 +0100

    Front + main.js

--- main.js
+++ main.js
@@ -1,19 +1,47 @@
-//let player = new Player('bob', 10);
-//console.log(player.getNom());
-var player1 = new Player("Jessy", 0);
-var player2 = new Player("Augustin", 0);
-var tray = new Tray(player1, player2, 4);
-var trayFactory = new TrayFactory(tray);
-var tabCell = trayFactory.createTray();
-console.log(tabCell);
+$(function () {
+    $('button.start').click(function () {
+        // Récupère les 2 Names
+        if ($('input:text.p1').attr('disabled')) {
+            return null;
+        }
 
-for (let i = 0; i < tray.getSize() * tray.getSize(); i) {
-    const ligne = document.createElement('tr');
-    for (let j = 0; j < tray.getSize(); j++) {
-        const colonne = document.createElement('td');
-        colonne.innerHTML = '<button value="' + i + '">' + tabCell[i][0].getValue() + '</button>';
-        ligne.appendChild(colonne);
-        i++;
-    }
-    document.getElementById('tray').appendChild(ligne);
-}
+        $('input:text.p1').attr('disabled', true);
+        $('input:text.p2').attr('disabled', true);
+
+        let p1Name = $('input:text.p1').val();
+        let p2Name = $('input:text.p2').val();
+
+        // Enlève le button start et on affiche le loader
+        $(this).css('display', 'none');
+        $('div.loader').css('display', 'block');
+
+        var player1 = new Player(p1Name, 0);
+        var player2 = new Player(p2Name, 0);
+        var tray = new Tray(player1, player2, 4);
+        var trayFactory = new TrayFactory(tray);
+        var tabCell = trayFactory.createTray();
+
+        console.log(tabCell);
+
+        // setTimeout pour avoir un effet de chargement
+        setTimeout(function () {
+            // Génère le rendu du tableau
+            for (let i = 0; i < tray.getSize() * tray.getSize(); i) {
+                const ligne = document.createElement('tr');
+                for (let j = 0; j < tray.getSize(); j++) {
+                    const colonne = document.createElement('td');
+                    colonne.innerHTML = '<button value="' + i + '" class="btn btn-info" style="width: 100%;">' + tabCell[i][0].getValue() + '</button>';
+                    ligne.appendChild(colonne);
+                    i++;
+                }
+                document.getElementById('tray').appendChild(ligne);
+            }
+
+            // Enlève le loader et remet le button start
+            $('button.start').css('display', 'block');
+            $('div.loader').css('display', 'none');
+        }, 500);
+
+
+    });
+})
