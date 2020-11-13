25f3b095e9814405963ef11cdaaa6ed4343a0c84
jessy.monnier <monnierjessy@gmail.com>
2020-11-13 15:48:51 +0100

    Ajout commentaire dans la classe TrayFactory

--- TrayFactory.js
+++ TrayFactory.js
@@ -1,3 +1,7 @@
+/**
+ * Constructeur de la classe TrayFactory
+ *
+ */
 function TrayFactory() {
     //this.tray = tray;
 }
@@ -6,10 +10,25 @@ function TrayFactory() {
     return this.tray;
 }*/
 
+/**
+ * Méthode qui va générer un nombre aléatoire
+ * 
+ * @param min Chiffre/Nombre minimum pour la génération aléatoire
+ * @param max Chiffre/Nombre maximum pour la génération aléatoire
+ *
+ * @return int Retourne un nombre aléatoire respectant les fonctionnalités du jeu
+ */
 TrayFactory.prototype.generateRandomNumber = function(min = -10, max = 15) {
     return Math.round(Math.random() * (max - min) + min);
 }
 
+/**
+ * Méthode qui va renvoyer un tableau contenant pour chaque indice une case (initialisation du tableau/ de son contenu)
+ * 
+ * @param tray Tableau qui va être généré
+ *
+ * @return int Retourne le tableau initialisé
+ */
 TrayFactory.prototype.createTray = function(tray) {
 
     var tabCell = [];
